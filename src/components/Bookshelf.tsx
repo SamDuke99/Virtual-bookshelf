99ee9e9e04f4754c58aea1f5515a6f77910e00eb
SamDuke99 <samueljduke99@gmail.com>
2025-04-03 12:29:33 +0100

    Revert "Added book detail modal on press"
    This reverts commit 0ecdb092d1fe8a191498794cb66e3dfe79e253a9.

--- src/components/Bookshelf.tsx
+++ src/components/Bookshelf.tsx
@@ -29,7 +29,6 @@ import {
   Color,
 } from "three";
 import { Book as StoreBook } from "../types";
-import BookDetailsModal from "./BookDetailsModal";
 
 interface SceneBook {
   id: string;
@@ -90,8 +89,6 @@ export default function Bookshelf({ books: storeBooks = [] }: BookshelfProps) {
   const [scene] = useState(() => new Scene());
   const [camera] = useState(() => new PerspectiveCamera(75, 1, 0.1, 1000));
   const [shelfBooks, setShelfBooks] = useState<SceneBook[]>([]);
-  const [selectedBook, setSelectedBook] = useState<StoreBook | null>(null);
-  const [isModalVisible, setIsModalVisible] = useState(false);
   const raycaster = useRef(new Raycaster());
   const mouse = useRef(new Vector2());
 
@@ -288,11 +285,6 @@ export default function Bookshelf({ books: storeBooks = [] }: BookshelfProps) {
 
       if (clickedBook) {
         console.log("Clicked book:", clickedBook.title);
-        const storeBook = storeBooks.find((book) => book.id === clickedBook.id);
-        if (storeBook) {
-          setSelectedBook(storeBook);
-          setIsModalVisible(true);
-        }
       }
     }
   };
@@ -355,14 +347,6 @@ export default function Bookshelf({ books: storeBooks = [] }: BookshelfProps) {
           activeOpacity={1}
         />
       </View>
-      <BookDetailsModal
-        book={selectedBook}
-        visible={isModalVisible}
-        onClose={() => {
-          setIsModalVisible(false);
-          setSelectedBook(null);
-        }}
-      />
     </View>
   );
 }
